9542a7e461e85031bad55849a28a4bea5514bd52
Felipe Dau <dau.felipe@gmail.com>
2015-10-18 21:26:41 -0200

    Implement serial reader to the RPi

--- blueberry/sensor.py
+++ blueberry/sensor.py
@@ -1,9 +1,13 @@
-from time import time
+from serial import Serial
+
+
+PORT = '/dev/ttyACM0'
+RATE = 9600
 
 
 class Sensor:
-    def __init__(self):
-        pass
+    def __init__(self, port=PORT, rate=RATE):
+        self.serial = Serial(port, rate)
 
     def read(self):
-        return time()
+        return self.serial.readline().strip()
